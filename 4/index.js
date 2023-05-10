9497174b9c5ef8fda00ee7aace64e0778764de64
csAfter40 <csafter40@gmail.com>
2023-05-10 11:02:27 +0300

    add imperative practice

---
+++ 4/index.js
@@ -0,0 +1,5 @@
+let container = document.querySelector("#root")
+const header = document.createElement('h1')
+header.textContent = "This is imperative!"
+header.className = "header"
+container.appendChild(header)
