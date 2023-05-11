4283cd50e82e3b97d31224b6b0a9b171c01b7116
csAfter40 <csafter40@gmail.com>
2023-05-12 00:52:31 +0300

    add new excercise

---
+++ 9/components/Main.js
@@ -0,0 +1,14 @@
+function Main() {
+    return (
+        <div>
+            <h1>This is main</h1>
+            <ul>
+                <li>item1</li>
+                <li>item2</li>
+                <li>item3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Main
