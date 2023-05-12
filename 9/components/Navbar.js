63c398436a8b18324dc087dd171bcd7d4c003309
csAfter40 <csafter40@gmail.com>
2023-05-12 09:26:14 +0300

    add navbar styling

--- 9/components/Navbar.js
+++ 9/components/Navbar.js
@@ -1,6 +1,12 @@
 function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <div className="nav-container">
+            <div className="nav-logo-container">
+                <img src="./images/react-logo-small.png" alt="React Logo" className="nav-logo"/>
+                <p className="nav-logo-text">ReactFacts</p>
+            </div>
+            <p className="nav-text">React Course - Project 1</p>
+        </div>
     )
 }
 
