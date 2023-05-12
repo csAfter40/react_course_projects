b50aa027e40e2a6a923bb9c2d9c2499f0f1d9f2f
csAfter40 <csafter40@gmail.com>
2023-05-12 20:32:19 +0300

    create airbnb project files and navbar

---
+++ AirBnb Experiences Project/components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div className="navbar-container">
+            <img src="./images/airbnb-logo.png" alt="AirBnb logo" />
+        </div>
+    )
+}
