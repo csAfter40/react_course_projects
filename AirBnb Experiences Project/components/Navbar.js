cd44bdab13fa35945fb8663862385c6c4de5f382
csAfter40 <csafter40@gmail.com>
2023-05-12 20:42:09 +0300

    fix navbar add fonts

--- AirBnb Experiences Project/components/Navbar.js
+++ AirBnb Experiences Project/components/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
-        <div className="navbar-container">
+        <nav className="navbar-container">
             <img src="./images/airbnb-logo.png" alt="AirBnb logo" />
-        </div>
+        </nav>
     )
 }
