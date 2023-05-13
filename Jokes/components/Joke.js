b1819dfc67e9b6b9b89b61a33db606670fc773e6
csAfter40 <csafter40@gmail.com>
2023-05-13 19:37:57 +0300

    refactor react conditional

--- Jokes/components/Joke.js
+++ Jokes/components/Joke.js
@@ -1,32 +1,12 @@
 export default function Joke(props) {
-    console.log(props.setup)
-    if (props.setup === undefined) {
-        return (
-            <div className="joke">
-                <div className="punchline">
-                    <p>{props.punchline}</p>
-                </div>
-            </div>
-        )
-    } else if (props.punchline === undefined) {
-        return (
-            <div className="joke">
-                <div className="setup">
-                    <p>{props.setup}</p>
-                </div>
-            </div>
-        )
-    } else {
-        return (
-            <div className="joke">
-                <div className="setup">
-                    <p>{props.setup}</p>
-                </div>
-                <div className="punchline">
-                    <p>{props.punchline}</p>
-                </div>
-            </div>
-        )
-    }
-    
+    return (
+        <div className="joke">
+            {props.setup && <div className="setup">
+                <p>{props.setup}</p>
+            </div>}
+            {props.punchline && <div className="punchline">
+                <p>{props.punchline}</p>
+            </div>}
+        </div>
+    )   
 }
