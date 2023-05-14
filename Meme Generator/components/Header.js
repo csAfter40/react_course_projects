e8e7942a13c4ff574eaf62a98a7a2706c43bdae7
csAfter40 <csafter40@gmail.com>
2023-05-15 01:10:32 +0300

    setup  project files and add header section

---
+++ Meme Generator/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="header-logo">
+                <img src="./images/troll-face.png" alt="troll face image" />
+                <h1 className="header-logo-text">Meme Generator</h1>
+            </div>
+            <h4 className="header-title">React Course - Project 3</h4>
+        </div>
+    )
+}
