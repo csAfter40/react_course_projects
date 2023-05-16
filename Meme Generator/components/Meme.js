2a5a729e2266aa637cbc081dbe9d219d9999dafc
csAfter40 <csafter40@gmail.com>
2023-05-16 23:04:52 +0300

    add meme image state

--- Meme Generator/components/Meme.js
+++ Meme Generator/components/Meme.js
@@ -1,12 +1,25 @@
+import { getRandomElement } from "../utils";
+import memeData from "../memesData"
+
 export default function Meme() {
+    const defaultImg = getRandomElement(memeData.data.memes);
+    const [memeImg, setMemeImg] = React.useState(<img src={defaultImg.url} alt="" />);
+    function handleButtonClick(event) {
+        let meme = getRandomElement(memeData.data.memes);
+        setMemeImg(<img src={meme.url} alt="meme image" />);
+    }
+
     return (
         <section className="form-container">
-            <div action="">
+            <div>
                 <div className="form-inputs">
                     <input id="upper-line-input" type="text" placeholder="Top Text" />
                     <input id="lower-line-input" type="text" placeholder="Bottom"/>
                 </div>
-                <button className="form-btn">Get a new meme image  🖼️</button>
+                <button onClick={handleButtonClick} className="form-btn">Get a new meme image  🖼️</button>
+            </div>
+            <div id="picture-container">
+                {memeImg}
             </div>
         </section>
     )
