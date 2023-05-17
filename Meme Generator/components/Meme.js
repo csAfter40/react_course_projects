626cb9e6a441ff4d3ebad04342bc2a69d227aa22
csAfter40 <csafter40@gmail.com>
2023-05-17 21:44:46 +0300

    refactor meme component with state

--- Meme Generator/components/Meme.js
+++ Meme Generator/components/Meme.js
@@ -2,12 +2,22 @@ import { getRandomElement } from "../utils";
 import memeData from "../memesData"
 
 export default function Meme() {
-    const defaultImg = getRandomElement(memeData.data.memes);
-    const [memeImg, setMemeImg] = React.useState(<img src={defaultImg.url} alt="" />);
+    const [allMemeImages, setAllMemeImages] = React.useState(memeData.data.memes);
+    const defaultImg = getRandomElement(allMemeImages);
+    const [meme, setMeme] = React.useState(
+        {
+            topText: "",
+            bottomText: "",
+            randomImage: defaultImg.url
+        }
+    );
     function handleButtonClick(event) {
-        let meme = getRandomElement(memeData.data.memes);
-        setMemeImg(<img src={meme.url} alt="meme image" />);
-    }
+        setMeme(prevMeme => ({
+            topText: prevMeme.topText,
+            bottomText: prevMeme.bottomText,
+            randomImage: getRandomElement(allMemeImages).url
+        }))
+    };
 
     return (
         <section className="form-container">
@@ -19,7 +29,7 @@ export default function Meme() {
                 <button onClick={handleButtonClick} className="form-btn">Get a new meme image  🖼️</button>
             </div>
             <div id="picture-container">
-                {memeImg}
+                <img src={meme.randomImage} alt="meme image" />
             </div>
         </section>
     )
