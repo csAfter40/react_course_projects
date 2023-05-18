e4b3b7620a61d3dfe72e9d04dc6f356f2d91b164
csAfter40 <csafter40@gmail.com>
2023-05-19 00:56:57 +0300

    place text on meme

--- Meme Generator/components/Meme.js
+++ Meme Generator/components/Meme.js
@@ -2,34 +2,60 @@ import { getRandomElement } from "../utils";
 import memeData from "../memesData"
 
 export default function Meme() {
-    const [allMemeImages, setAllMemeImages] = React.useState(memeData.data.memes);
-    const defaultImg = getRandomElement(allMemeImages);
-    const [meme, setMeme] = React.useState(
+    const allMemeImages = memeData.data.memes;
+    const defaultImage = getRandomElement(allMemeImages);
+    const [formData, setFormData] = React.useState(
         {
             topText: "",
             bottomText: "",
-            randomImage: defaultImg.url
+            image: defaultImage.url
         }
     );
     function handleButtonClick(event) {
-        setMeme(prevMeme => ({
-            topText: prevMeme.topText,
-            bottomText: prevMeme.bottomText,
-            randomImage: getRandomElement(allMemeImages).url
+        setFormData(prevData => ({
+            topText: prevData.topText,
+            bottomText: prevData.bottomText,
+            image: getRandomElement(allMemeImages).url
         }))
     };
+    function handleChange(event) {
+        const {name, value} = event.target
+        setFormData(prevData => ({
+            topText: prevData.topText,
+            bottomText: prevData.bottomText,
+            image: prevData.image,
+            [name]: value
+        }))
+    }
+    console.log(formData)
 
     return (
         <section className="form-container">
             <div>
                 <div className="form-inputs">
-                    <input id="upper-line-input" type="text" placeholder="Top Text" />
-                    <input id="lower-line-input" type="text" placeholder="Bottom"/>
+                    <input 
+                        id="upper-line-input" 
+                        type="text" 
+                        placeholder="Top Text" 
+                        name="topText"
+                        value={formData.topText}
+                        onChange={handleChange}
+                        />
+                    <input 
+                        id="lower-line-input" 
+                        type="text" 
+                        placeholder="Bottom"
+                        name="bottomText"
+                        value={formData.bottomText}
+                        onChange={handleChange}
+                    />
                 </div>
                 <button onClick={handleButtonClick} className="form-btn">Get a new meme image  🖼️</button>
             </div>
             <div id="picture-container">
-                <img src={meme.randomImage} alt="meme image" />
+                <img src={formData.image} alt="meme image" />
+                <h2 className="meme-text top">{formData.topText}</h2>
+                <h2 className="meme-text bottom">{formData.bottomText}</h2>
             </div>
         </section>
     )
