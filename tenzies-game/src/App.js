c67095212081e0ff1ff4d9fec9719c77fbe5aa38
csAfter40 <csafter40@gmail.com>
2023-05-27 08:45:00 +0300

    start project

---
+++ tenzies-game/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="main-frame">
+      <div className='main-container'>
+        <h1>game here</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
